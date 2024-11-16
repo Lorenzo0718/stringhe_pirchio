f2a6e6b02feb2ede2e6f35520a13116a0f50d4d0
Lorenzo0718 <lorenzopirchio07@gmail.com>
2024-11-16 16:16:52 +0100

    Add files via upload

---
+++ Esercizio_stringhe.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.esercizio_stringhe;
+
+/**
+ *
+ * Pirchio Lorenzo 4.inf.D 12/11/2024
+ */
+public class Esercizio_stringhe {
+
+    public static void main(String[] args) {
+        //Instazazione di un oggetto per il richiamo delle funzioni appartenenti della classe Input.
+        Input Input = new Input();
+        
+        Input.scan();
+        
+        //Instazazione di un oggetto per il richiamo delle funzioni appartenenti della classe ricerca_A.
+        ricerca_A cercaA = new ricerca_A(Input);
+        
+        cercaA.trova_a(Input);
+        
+        cercaA.stampa();
+
+    }
+}
